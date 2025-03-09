6c4fdd4f0be5064ae3baf5fae099411d593c77ae
KCCPMG <39074889+KCCPMG@users.noreply.github.com>
2025-03-09 14:49:37 -0700

    Time: 131 ms (4.84%) | Memory: 65.9 MB (5.75%) - LeetSync

---
+++ merge-two-sorted-lists/21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+
+    console.log(list1, list2);
+    if (list1 === null && list2 === null) return null;
+
+
+    let head = new ListNode();
+    if (list1 || list2) {
+        if (list1 && (!list2  || list1.val < list2.val)) {
+            head.val = list1.val;
+            list1 = list1.next;
+        } else {
+            head.val = list2.val;
+            list2 = list2.next;
+        }
+        console.log(head, list1, list2);
+    }
+    let cursor = head;
+    while (list1 || list2) {
+        if (list1 && (!list2  || list1.val < list2.val)) {
+            cursor.next = new ListNode(list1.val);
+            cursor = cursor.next;
+            list1 = list1.next;
+        } else {
+            cursor.next = new ListNode(list2.val);
+            cursor = cursor.next;
+            list2 = list2.next;
+        }
+        console.log(head, list1, list2);
+    }
+    return head;
+};
