bb3b51387a315594d603cfae8c8ba222f4aea288
KCCPMG <39074889+KCCPMG@users.noreply.github.com>
2025-03-11 15:32:48 -0700

    Time: 1 ms (63.43%) | Memory: 58.5 MB (7.14%) - LeetSync

--- merge-two-sorted-lists/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ merge-two-sorted-lists/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -4,11 +4,11 @@
  */
 var removeDuplicates = function(nums) {
     let i = nums.length-1;
-    let dels = [];
+    // let dels = [];
     while (i > 0) {
-        if (nums[i] == nums[i-1]) dels.push(i);
+        if (nums[i] == nums[i-1]) nums.splice(i,1);
         i--;
     }
-    dels.forEach(d => nums.splice(d, 1));
+    // dels.forEach(d => nums.splice(d, 1));
     return nums.length;
 };
