c5013bc96edefce913e3b17055229fbaf1424b74
KCCPMG <39074889+KCCPMG@users.noreply.github.com>
2025-03-11 15:52:09 -0700

    Time: 0 ms (100.00%) | Memory: 57.8 MB (19.60%) - LeetSync

--- merge-two-sorted-lists/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ merge-two-sorted-lists/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,6 +3,12 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+    // let i = nums.length-1;
+    // while (i > 0) {
+    //     if (nums[i] == nums[i-1]) nums.splice(i,1);
+    //     i--;
+    // }
+    // return nums.length;
     nums.splice(0, nums.length, ...new Set(nums));
     return nums.length;
 };
