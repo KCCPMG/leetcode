8db0d21a90ee375e49943270f663d1b9e8b07c66
Connor Wales <39074889+KCCPMG@users.noreply.github.com>
2025-05-08 01:34:38 -0700

    Time: 60 ms (12.02%) | Memory: 63.3 MB (14.39%) - LeetSync

--- zigzag-conversion/6-zigzag-conversion/zigzag-conversion.js
+++ zigzag-conversion/6-zigzag-conversion/zigzag-conversion.js
@@ -1,51 +1,94 @@
-3 > 4
-4 > 6
-5 > 8
+// /**
+//  * @param {string} s
+//  * @param {number} numRows
+//  * @return {string}
+//  */
+// var convert = function(s, numRows) {
+//     // word goes straight down, then up/right, then down
+//     // a character will go up/right by going up one row and
+//     // right one space, there is always at least a minimum of 
+//     // one space between characters
 
-/**
- * @param {string} s
- * @param {number} numRows
- * @return {string}
- */
+//     if (numRows === 1) return s;
+//     const rows = new Array(numRows).fill("");
+//     console.log(s, rows);
+
+//     for (let i=0; i<numRows && i<s.length; i++) {
+//         let indices = []
+//         if (i === 0 || i === (numRows - 1)) {
+//             let spacing = numRows * 2 - 2;
+//             for (let c=i; c<s.length; c+=spacing) {
+//                 indices.push(c);
+//             }
+//         } else {
+//             let firstSpacing = (numRows - 1 - i) * 2;
+//             let secondSpacing = numRows * 2 - 2;
+//             let lookahead = i;
+//             indices.push(lookahead);
+//             while (lookahead < s.length) {
+//                 lookahead += firstSpacing;
+//                 if (lookahead < s.length) indices.push(lookahead);
+//                 lookahead = lookahead - firstSpacing + secondSpacing;
+//                 if (lookahead < s.length) indices.push(lookahead);
+//             }
+//         }
+
+//         console.log(indices);
+
+//         for (let index of indices) {
+//             rows[i] += s[index];
+//         }
+//         console.log(rows[i])
+//     } 
+
+
+//     return rows.join("");
+
+
+
+
+// }
+
+// 0 > 0, 6
+// 1 > 1, 5
+// 2 > 2, 4
+// 3 > 3
+
+
 var convert = function(s, numRows) {
-    // word goes straight down, then up/right, then down
-    // a character will go up/right by going up one row and
-    // right one space, there is always at least a minimum of 
-    // one space between characters
 
     if (numRows === 1) return s;
-    const rows = new Array(numRows).fill("");
-    console.log(s, rows);
+    
+    stringArray = new Array(numRows).fill("");
+    console.log(stringArray);
+    let loopSize = numRows * 2 - 2;
+    let maxRowIndex = numRows - 1;
 
-    for (let i=0; i<numRows && i<s.length; i++) {
-        let indices = []
-        if (i === 0 || i === (numRows - 1)) {
-            let spacing = numRows * 2 - 2;
-            for (let c=i; c<s.length; c+=spacing) {
-                indices.push(c);
+    for (let stringIndex=0; stringIndex<s.length; stringIndex++) {
+
+        let currentlyExaminingRowIndex = 0;
+
+        while (currentlyExaminingRowIndex < numRows) {
+            let positionInLoop = stringIndex % loopSize;
+            let lowerCandidate = currentlyExaminingRowIndex;
+            let upperCandidate = loopSize - currentlyExaminingRowIndex;
+            // console.log({
+            //     stringIndex,
+            //     character: s[stringIndex],
+            //     currentlyExaminingRowIndex,
+            //     positionInLoop,
+            //     lowerCandidate,
+            //     upperCandidate
+            // })
+            if (positionInLoop === lowerCandidate || positionInLoop === upperCandidate) {
+                stringArray[currentlyExaminingRowIndex] += s[stringIndex];
+                break;
             }
-        } else {
-            let firstSpacing = (numRows - 1 - i) * 2;
-            let secondSpacing = numRows * 2 - 2;
-            let lookahead = i;
-            indices.push(lookahead);
-            while (lookahead < s.length) {
-                lookahead += firstSpacing;
-                if (lookahead < s.length) indices.push(lookahead);
-                lookahead = lookahead - firstSpacing + secondSpacing;
-                if (lookahead < s.length) indices.push(lookahead);
-            }
+
+            currentlyExaminingRowIndex++;
         }
 
-        console.log(indices);
 
-        for (let index of indices) {
-            rows[i] += s[index];
-        }
-        console.log(rows[i])
-    } 
-
-
-    return rows.join("");
-
+    }
+    return stringArray.join("");
 }
